a64e075c3ade492630713774ab4cfbbc8cde8e40
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-04-04 22:19:34 +0100

    added new admin table resource for adding product items

--- noisyatom/catalog/admin.py
+++ noisyatom/catalog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, ProductItem
 
 # Registering category and products
 
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(ProductItem)
