59525516914c2aa6c584cb1ea6bbba185d693383
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-11-02 10:10:26 +0000

    Adding dynamic text to views

--- noisyatom/cms/views.py
+++ noisyatom/cms/views.py
@@ -19,5 +19,9 @@ def dashboard_view(request):
 
 
 def about_us(request):
-    context={}
+    #TODO Make this dynamic content from a data base.
+    context={'title':'About Us',
+                'paragraph1': 'We have been active for 4 years. A dynamic tech focussed company. We love technology. Our core competences are in Python and Python web frameworks.',
+                'paragraph2': 'Check back soon for our open source products centred around QR Code generation and mapping with OAuth2 services.',
+                'paragraph3': 'For now - read more to meet the team.'}
     return render(request, 'coming-soon.html', context)
