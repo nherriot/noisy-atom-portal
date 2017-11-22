c657b2e78004d29278a1674049b04615956030d4
Nicholas Herriot <Nicholas.Herriot@vodafone.com>
2017-11-22 23:40:37 +0000

    adding a test case of a user logged in

--- noisyatom/blogs/tests/test_views.py
+++ noisyatom/blogs/tests/test_views.py
@@ -1,12 +1,21 @@
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
 
 
 from blogs.models import Post
 
 
 class PostModelTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        test_user1 = User.objects.create_user(username='testuser1', password='12345')
+        test_user1.save()
+        test_user2 = User.objects.create_user(username='testuser2', password='12345')
+        test_user2.save()
 
     def create_post(self, title='Post new blog'):
         return Post.objects.create(title=title)
@@ -35,6 +44,24 @@ class PostModelTest(TestCase):
         
         self.assertEqual(response.status_code, 404)
 
+    def test_update_blog_post_user_loged_in(self):
+        '''
+            Login our user, create a blog post and make sure we can get the blog post data from the page
+        '''
+        obj = self.create_post(title='Some new title for new test')     # This should go in the setup function
+        print("***** We have the following users: {}".format(User.objects.all()))  # returns []
+        login = self.client.login(username='testuser1', password='12345')
+
+
+        print("***** We have logged in now and have the following users: {}".format(User.objects.all()))  # returns one user
+        print("******The first index user has a login state of: {}".format(User.objects.all()[0].is_authenticated()))  # returns True
+
+
+        url = reverse('blogs:updated', kwargs={'slug': obj.slug})
+        print("***** We are looking up the URL: {}".format(url))
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
 
     def test_delete_views(self):
         obj = self.create_post(title='Some new title for new test')
