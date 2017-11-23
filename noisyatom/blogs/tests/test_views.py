27cc116d8f82c7472639ed4cc494cfeca09c2adf
Nicholas Herriot <Nicholas.Herriot@vodafone.com>
2017-11-23 16:05:38 +0000

    fixing tabs in file for views.py. adding more test cases

--- noisyatom/blogs/tests/test_views.py
+++ noisyatom/blogs/tests/test_views.py
@@ -12,31 +12,44 @@ class PostModelTest(TestCase):
 
     def setUp(self):
         self.client = Client()
-        test_user1 = User.objects.create_user(username='testuser1', password='12345')
-        test_user1.save()
-        test_user2 = User.objects.create_user(username='testuser2', password='12345')
-        test_user2.save()
+        test_user_normal = User.objects.create_user(username='testuser_normal', password='12345')
+        test_user_staff = User.objects.create_user(username='testuser_staff', password='12345', is_staff=True)
+        test_user_admin = User.objects.create_user(username='testuser_admin', password='12345', is_superuser=True)
+        test_user_inactive = User.objects.create_user(username='testuser_inactive', password='12345', is_active=False, is_staff=True, is_superuser=True)
+        test_user_normal.save()
+        test_user_staff.save()
+        test_user_admin.save()
+        test_user_inactive.save()
 
     def create_post(self, title='Post new blog'):
         return Post.objects.create(title=title)
 
-    def test_lis_views(self):
+    def test_list_views(self):
+        '''
+            This will request a list of all the blog titles to be rendered on the page. We need to check that all the
+            blog titles created in SETUP get displayed on the page.
+        '''
         url = reverse('blogs:list')
         response = self.client.get(url)
+        #TODO YOu need to check that the tiles are present in the list Dilshad. You are only looking for the http200
         self.assertEqual(response.status_code, 200)
 
 
     def test_detail_views(self):
+        '''
+            This will display a blog post and it's contents. We need to check that a blog post can be displayed on
+            a page. Blog title and description saved in setup should be present in the HTML rendered on the page.
+        '''
         obj = self.create_post(title='Some new title for new test')
         response = self.client.get(obj.get_absolute_url())
+        #TODO You need to check that the description and title are present in the html returned from the server Dilshad
         self.assertEqual(response.status_code, 200)
 
 
-    def test_update_views(self):
+    def test_update_blog_post_anonymous_user(self):
         '''
-            The reason of why we use 404 because in the  update_post I am using the if statment 
-            say if not request.user.is_staff or superuser than get 404 if I change the if statment to
-            if request.user.is_staff or superuser than change the 404 to 200 the same for the delete method
+            An anonymous user tries to update a blog post. This should return a 404 and fail. Only a super user
+            or staff user can update blogs!
         '''
         obj = self.create_post(title='Some new title for new test')
         url = reverse('blogs:updated', kwargs={'slug': obj.slug})
@@ -44,24 +57,82 @@ class PostModelTest(TestCase):
         
         self.assertEqual(response.status_code, 404)
 
-    def test_update_blog_post_user_loged_in(self):
+    def test_update_blog_post_user_logged_in(self):
         '''
-            Login our user, create a blog post and make sure we can get the blog post data from the page
+            Login our user, and try create a blog post and make sure we get a http404. Only staff and admin
+            can update blogs!
         '''
         obj = self.create_post(title='Some new title for new test')     # This should go in the setup function
         print("***** We have the following users: {}".format(User.objects.all()))  # returns []
-        login = self.client.login(username='testuser1', password='12345')
+        login = self.client.login(username='testuser_normal', password='12345')
+        if login:
+            url = reverse('blogs:updated', kwargs={'slug': obj.slug})
+            response = self.client.get(url)
+
+            self.assertEqual(response.status_code, 404)
+            self.client.logout()
+        else:
+            self.assertRaises('login failed')
+
+    def test_update_blog_post_as_staff_user(self):
+        '''
+            Login our user, and try create a blog post and make sure we get a http404. Only staff and admin
+            can update blogs!
+        '''
+        obj = self.create_post(title='Some new title for new test')     # This should go in the setup function
+        print("***** We have the following users: {}".format(User.objects.all()))  # returns []
+        login = self.client.login(username='testuser_staff', password='12345')
+        if login:
+            url = reverse('blogs:updated', kwargs={'slug': obj.slug})
+            print("***** We are looking up the URL: {}".format(url))
+            response = self.client.get(url)
+
+            self.assertEqual(response.status_code, 200)
+            self.client.logout()
+        else:
+            self.assertRaises('login failed')
+
+    # def test_update_blog_post_as_admin_user(self):
+    #     '''
+    #         Login our user, and try create a blog post and make sure we get a http404. Only staff and admin
+    #         can update blogs!
+    #     '''
+    #     obj = self.create_post(title='Some new title for new test')     # This should go in the setup function
+    #     print("***** We have the following users: {}".format(User.objects.all()))  # returns []
+    #     login = self.client.login(username='testuser_normal', password='12345')
+    #
+    #     print("The login object is: {}".format(login))
+    #
+    #
+    #     url = reverse('blogs:updated', kwargs={'slug': obj.slug})
+    #     print("***** We are looking up the URL: {}".format(url))
+    #     response = self.client.get(url)
+    #
+    #     self.assertEqual(response.status_code, 200)
+    #     self.client.logout()
+    #
+    # def test_update_blog_post_as_inactive_user(self):
+    #     '''
+    #         Login our user, and try create a blog post and make sure we get a http404. Even if the user is
+    #         staff or admin or both! They must be active to be able to use the system. Hence the can't update blogs!
+    #     '''
+    #     obj = self.create_post(title='Some new title for new test')     # This should go in the setup function
+    #     print("***** We have the following users: {}".format(User.objects.all()))  # returns []
+    #     login = self.client.login(username='testuser_inactive', password='12345')
+    #
+    #     print("The login object is: {}".format(login))
+    #     print("***** We have logged in now and have the following users: {}".format(User.objects.all()))  # returns one user
+    #     print("******The first index user has a login state of: {}".format(User.objects.all()[0].is_authenticated()))  # returns True
+    #
+    #
+    #     url = reverse('blogs:updated', kwargs={'slug': obj.slug})
+    #     print("***** We are looking up the URL: {}".format(url))
+    #     response = self.client.get(url)
+    #
+    #     self.assertEqual(response.status_code, 200)
+    #     self.client.logout()
 
 
-        print("***** We have logged in now and have the following users: {}".format(User.objects.all()))  # returns one user
-        print("******The first index user has a login state of: {}".format(User.objects.all()[0].is_authenticated()))  # returns True
-
-
-        url = reverse('blogs:updated', kwargs={'slug': obj.slug})
-        print("***** We are looking up the URL: {}".format(url))
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 200)
 
     def test_delete_views(self):
         obj = self.create_post(title='Some new title for new test')
--- noisyatom/blogs/views.py
+++ noisyatom/blogs/views.py
@@ -12,116 +12,116 @@ from .models import Post
 
 
 def create_post(request):
-	template_name = 'create.html'
+    template_name = 'create.html'
 
-	if not request.user.is_staff or not request.user.is_superuser:
-		raise Http404
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
 
-	form = PostForm(request.POST or None, request.FILES or None)
-	if form.is_valid():
-		instance = form.save(commit=False) 
-		instance.user = request.user
-		instance.save()
-		messages.success(request, 'The page successfully created')
-		return HttpResponseRedirect(instance.get_absolute_url())
-	else:
-		messages.error(request, 'The page is not created')
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.user = request.user
+        instance.save()
+        messages.success(request, 'The page successfully created')
+        return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, 'The page is not created')
 
-	context = {
-		'form': form,
-	}
-	return render(request, template_name, context)
+    context = {
+        'form': form,
+    }
+    return render(request, template_name, context)
 
 #retrieve
 def detail_post(request, slug=None):
-	template_name = 'details.html'
+    template_name = 'details.html'
 
-	instance = get_object_or_404(Post, slug=slug)
-	if instance.draft or instance.publish > timezone.now().date():
-		if not request.user.is_staff or not request.user.is_superuser:
-			raise Http404
-	share_string = quote_plus(instance.content)
+    instance = get_object_or_404(Post, slug=slug)
+    if instance.draft or instance.publish > timezone.now().date():
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
+    share_string = quote_plus(instance.content)
 
-	context = {
-		'title': instance.title,
-		'instance': instance,
-		'share_string': share_string,
-	}
+    context = {
+        'title': instance.title,
+        'instance': instance,
+        'share_string': share_string,
+    }
 
-	return render(request, template_name, context)
+    return render(request, template_name, context)
 
 
 def list_post(request):
-	template_name = 'list.html'
+    template_name = 'list.html'
 
-	today = timezone.now().date()
-	queryset_list = Post.objects.active().order_by('-created')
+    today = timezone.now().date()
+    queryset_list = Post.objects.active().order_by('-created')
 
-	if request.user.is_staff or request.user.is_superuser:
-		queryset_list = Post.objects.all().order_by('-created')
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all().order_by('-created')
 
-	query = request.GET.get('q')
-	
-	if query:
-		queryset_list = queryset_list.filter(
-				Q(title__icontains=query) |
-				Q(content__icontains=query) |
-				Q(user__first_name__icontains=query) | 
-				Q(user__last_name__icontains=query) 
-			).distinct()
-	
-	paginator = Paginator(queryset_list, 2)
+    query = request.GET.get('q')
 
-	page = request.GET.get('page')
-	try:
-	    queryset = paginator.page(page)
-	except PageNotAnInteger:
-	    # If page is not an integer, deliver first page.
-	    queryset = paginator.page(1)
-	except EmptyPage:
-	    # If page is out of range (e.g. 9999), deliver last page of results.
-	    queryset = paginator.page(paginator.num_pages)
+    if query:
+        queryset_list = queryset_list.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+            ).distinct()
+
+    paginator = Paginator(queryset_list, 2)
+
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
 
 
-	context = {
-			'object_list': queryset,
-			'today': today,
-		}
+    context = {
+            'object_list': queryset,
+            'today': today,
+        }
 
-	return render(request, template_name, context)
+    return render(request, template_name, context)
 
 # update
 def update_post(request, slug=None):
-	template_name = 'update.html'
+    template_name = 'update.html'
 
-	if not request.user.is_staff or not request.user.is_superuser:
-		raise Http404
-	instance = get_object_or_404(Post, slug=slug)
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    instance = get_object_or_404(Post, slug=slug)
 
-	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
-	if form.is_valid():
-		instance = form.save(commit=False) 
-		instance.save()
-		messages.success(request, 'The page successfully updated')
-		return HttpResponseRedirect(instance.get_absolute_url())
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, 'The page successfully updated')
+        return HttpResponseRedirect(instance.get_absolute_url())
 
-	context = {
-		'title': instance.title,
-		'instance': instance.content,
-		'slug': instance.slug,
-		'image': instance.image,
-		'form': form,
-	}
+    context = {
+        'title': instance.title,
+        'instance': instance.content,
+        'slug': instance.slug,
+        'image': instance.image,
+        'form': form,
+    }
 
-	return render(request, template_name, context)
+    return render(request, template_name, context)
 
 # from django.views.decorators.csrf import csrf_exempt
 
 # @csrf_exempt
 def delete_post(request, slug=None):
-	if not request.user.is_staff or not request.user.is_superuser:
-		raise Http404
-	instance = get_object_or_404(Post, slug=slug)
-	instance.delete()
-	messages.success(request, 'The post has been deleted')
-	return redirect('blogs:list')
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    instance = get_object_or_404(Post, slug=slug)
+    instance.delete()
+    messages.success(request, 'The post has been deleted')
+    return redirect('blogs:list')
