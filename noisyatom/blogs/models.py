e3e89d322b1d32bef22894fbfc2a17d910e4ce53
Dilshad Abdulla <Dilshad.Abdulla@outlook.com>
2017-11-30 09:46:51 +0000

    Removed duplicated fields in the models

--- noisyatom/blogs/models.py
+++ noisyatom/blogs/models.py
@@ -39,16 +39,6 @@ class Post(models.Model):
         						width_field='width_field')
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    content = models.TextField()
-    image = models.ImageField(upload_to=upload_image_location,
-                                null=True, blank=True,
-                                height_field='height_field',
-                                width_field='width_field')
-    height_field = models.IntegerField(default=0)
-    width_field = models.IntegerField(default=0)
 
     draft = models.BooleanField(default=False)
     publish = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
