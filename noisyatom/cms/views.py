9495bceec92caf2a6dafc5cd56f0618167e63950
Nicholas Herriot <Nicholas.Herriot@vodafone.com>
2017-12-09 20:42:04 +0000

    adding titles to the blog views. they are partially dynamic in that they are added to the context in the responses.

--- noisyatom/cms/views.py
+++ noisyatom/cms/views.py
@@ -10,10 +10,11 @@ def index_view(request):
 
 
 def coming_soon(request):
-    context={'title': 'Coming Soon',
-                'paragraph1': 'We are working hard!',
-                'paragraph2': 'Stay tuned for our QR codes!',
-                'paragraph3': ''}
+    context={
+        'main_title': 'Coming Soon',
+        'paragraph1': 'We are working hard!',
+        'paragraph2': 'Stay tuned for our QR codes!',
+        'paragraph3': ''}
 
     return render(request, 'sub-header.html', context)
 
@@ -28,7 +29,7 @@ def alfa_view(request):
     template_name = 'alfa.html'
     # TODO Make this dynamic data from a table in our database
     context = {
-        'title': 'Ecommerce Platform for Alfa Aesar Ltd',
+        'main_title': 'Ecommerce Platform for Alfa Aesar Ltd',
         'description1':'This project encompassed the migration of a legacy "Websmart" system into a modern python Django web platform. The platform was deployed onto a hosted server running with Redhat SE Linux 6.2 on an IBM PowerPC platform.',
         'description2':'Read on to find out more about the specifics...',
         'description3':''
@@ -38,8 +39,9 @@ def alfa_view(request):
 
 def about_us(request):
     # TODO Make this dynamic content from a data base.
-    context={'title':'About Us',
-                'paragraph1': 'We have been active for 4 years. A dynamic tech focussed company. We love technology. Our core competences are in Python and Python web frameworks.',
-                'paragraph2': 'Check back soon for our open source products centred around QR Code generation and mapping with OAuth2 services.',
-                'paragraph3': 'For now - read more to meet the team.'}
+    context={
+        'main_title':'About Us',
+        'paragraph1': 'We have been active for 4 years. A dynamic tech focussed company. We love technology. Our core competences are in Python and Python web frameworks.',
+        'paragraph2': 'Check back soon for our open source products centred around QR Code generation and mapping with OAuth2 services.',
+        'paragraph3': 'For now - read more to meet the team.'}
     return render(request, 'about-us.html', context)
